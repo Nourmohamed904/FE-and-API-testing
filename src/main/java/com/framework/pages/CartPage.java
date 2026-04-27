029b582a24bbf6161ff9b331d59a6e6152f06e2d
Dell <osamashahd252@gmail.com>
2026-04-27 20:21:19 +0300

    checkout

--- src/main/java/com/framework/pages/CartPage.java
+++ src/main/java/com/framework/pages/CartPage.java
@@ -13,13 +13,11 @@ public class CartPage extends BasePage {
     }
 
     private final By cartTableRows = By.cssSelector(".table-bordered tbody tr");
-    private final By productNameColumn = By.xpath(".//td[@class='text-left']/a");
-    private final By priceColumn = By.xpath(".//td[@class='text-right']");
-    private final By totalPrice = By.xpath("//table[@class='table table-bordered']//tfoot//td[contains(text(),'Total')]/following-sibling::td");
+    private final By productNameColumn = By.cssSelector(".text-left a");
     private final By checkoutButton = By.linkText("Checkout");
-    private final By removeButton = By.cssSelector(".btn-danger");
-    private final By quantityField = By.cssSelector(".form-control");
-    private final By updateButton = By.cssSelector(".btn-primary");
+    private final By cartSubtotal = By.cssSelector(".table-bordered tfoot tr:first-child td:last-child");
+    private final By cartTotalValue = By.cssSelector(".table-bordered tfoot tr:last-child td:last-child");
+    private final By shoppingCartHeading = By.cssSelector("#content h1");
 
     public boolean isProductInCart(String productName) {
         List<WebElement> rows = driver.findElements(cartTableRows);
@@ -36,14 +34,23 @@ public class CartPage extends BasePage {
         List<WebElement> rows = driver.findElements(cartTableRows);
         for (WebElement row : rows) {
             if (row.findElement(productNameColumn).getText().equalsIgnoreCase(productName)) {
-                return row.findElement(priceColumn).getText();
+                List<WebElement> priceCells = row.findElements(By.cssSelector(".text-right"));
+                return priceCells.size() > 0 ? priceCells.get(0).getText() : null;
             }
         }
         return null;
     }
 
-    public String getTotalPrice() {
-        return getText(totalPrice);
+    public String getProductPriceInCart(String productName) {
+        return getProductPrice(productName);
+    }
+
+    public String getCartSubtotal() {
+        return getText(cartSubtotal);
+    }
+
+    public String getCartTotalValue() {
+        return getText(cartTotalValue);
     }
 
     public CheckoutPage proceedToCheckout() {
@@ -57,5 +64,9 @@ public class CartPage extends BasePage {
 
     public boolean isCartEmpty() {
         return getCartItemCount() == 0;
+    }
+
+    public boolean isCartPageDisplayed() {
+        return isDisplayed(shoppingCartHeading);
     }
 }
--- src/main/java/com/framework/pages/CheckoutPage.java
+++ src/main/java/com/framework/pages/CheckoutPage.java
@@ -3,6 +3,7 @@ package com.framework.pages;
 import com.framework.base.BasePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
 public class CheckoutPage extends BasePage {
@@ -11,24 +12,49 @@ public class CheckoutPage extends BasePage {
         super(driver);
     }
 
-    private final By billingFirstName = By.id("input-payment-firstname");
-    private final By billingLastName = By.id("input-payment-lastname");
-    private final By billingAddress1 = By.id("input-payment-address-1");
-    private final By billingCity = By.id("input-payment-city");
-    private final By billingPostcode = By.id("input-payment-postcode");
-    private final By billingCountry = By.id("input-payment-country");
-    private final By billingZone = By.id("input-payment-zone");
-    private final By billingContinueBtn = By.id("button-payment-address");
-    private final By shippingContinueBtn = By.id("button-shipping-address");
-    private final By flatShippingRate = By.cssSelector("input[name='shipping_method'][value='flat.flat']");
-    private final By shippingMethodContinue = By.id("button-shipping-method");
-    private final By commentsTextarea = By.name("comment");
-    private final By termsAgreeCheckbox = By.name("agree");
-    private final By paymentMethodContinue = By.id("button-payment-method");
-    private final By confirmOrderBtn = By.id("button-confirm");
+    // Billing details
+    private final By billingFirstName = By.cssSelector("#input-payment-firstname");
+    private final By billingLastName = By.cssSelector("#input-payment-lastname");
+    private final By billingAddress1 = By.cssSelector("#input-payment-address-1");
+    private final By billingCity = By.cssSelector("#input-payment-city");
+    private final By billingPostcode = By.cssSelector("#input-payment-postcode");
+    private final By billingCountry = By.cssSelector("#input-payment-country");
+    private final By billingZone = By.cssSelector("#input-payment-zone");
+    private final By billingContinueBtn = By.cssSelector("#button-payment-address");
+
+    // Shipping details
+    private final By shippingFirstName = By.cssSelector("#input-shipping-firstname");
+    private final By shippingLastName = By.cssSelector("#input-shipping-lastname");
+    private final By shippingAddress1 = By.cssSelector("#input-shipping-address-1");
+    private final By shippingCity = By.cssSelector("#input-shipping-city");
+    private final By shippingPostcode = By.cssSelector("#input-shipping-postcode");
+    private final By shippingCountry = By.cssSelector("#input-shipping-country");
+    private final By shippingZone = By.cssSelector("#input-shipping-zone");
+    private final By shippingContinueBtn = By.cssSelector("#button-shipping-address");
+
+    // Shipping method
+    private final By flatShippingRate = By.cssSelector("input[value='flat.flat']");
+    private final By shippingMethodContinue = By.cssSelector("#button-shipping-method");
+
+    // Payment method
+    private final By commentsTextarea = By.cssSelector("textarea[name='comment']");
+    private final By termsAgreeCheckbox = By.cssSelector("input[name='agree']");
+    private final By paymentMethodContinue = By.cssSelector("#button-payment-method");
+
+    // Confirm order
+    private final By confirmOrderBtn = By.cssSelector("#button-confirm");
     private final By orderSuccessMessage = By.cssSelector("#content h1");
+
+    // Address options
     private final By newAddressRadio = By.cssSelector("input[value='new']");
-    private final By sameAddressRadio = By.cssSelector("input[value='same']");
+    private final By addressDropdown = By.cssSelector("#input-payment-address");
+
+    // Confirm order table
+    private final By confirmOrderTotal = By.cssSelector(".table-bordered tfoot tr:last-child td:last-child");
+    private final By flatShippingRateText = By.cssSelector(".table-bordered td:contains('Flat Shipping Rate') + td");
+
+    // Cart widget
+    private final By cartTotalWidget = By.cssSelector("#cart-total");
 
     public CheckoutPage fillBillingDetails(String firstName, String lastName,
                                            String address, String city,
@@ -54,12 +80,38 @@ public class CheckoutPage extends BasePage {
         return this;
     }
 
-    public CheckoutPage useSameBillingAddress() {
-        if (areElementsPresent(sameAddressRadio)) {
-            click(sameAddressRadio);
+    public CheckoutPage fillShippingDetails(String firstName, String lastName,
+                                            String address, String city,
+                                            String postcode, String country, String zone) {
+        if (areElementsPresent(newAddressRadio)) {
+            click(newAddressRadio);
         }
+
+        waitForElement(shippingFirstName);
+        type(shippingFirstName, firstName);
+        type(shippingLastName, lastName);
+        type(shippingAddress1, address);
+        type(shippingCity, city);
+        type(shippingPostcode, postcode);
+
+        Select countrySelect = new Select(waitForElement(shippingCountry));
+        countrySelect.selectByVisibleText(country);
+
+        wait.until(d -> d.findElement(shippingZone).isEnabled());
+        Select zoneSelect = new Select(driver.findElement(shippingZone));
+        zoneSelect.selectByVisibleText(zone);
+
         click(shippingContinueBtn);
         return this;
+    }
+
+    public boolean isAddressDropdownPopulated() {
+        try {
+            Select select = new Select(driver.findElement(addressDropdown));
+            return select.getOptions().size() > 0;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     public CheckoutPage selectFlatShippingRate() {
@@ -96,5 +148,34 @@ public class CheckoutPage extends BasePage {
 
     public boolean isOrderPlaced() {
         return getOrderSuccessMessage().contains("Your order has been placed");
+    }
+
+    public boolean isDeliveryMethodSectionDisplayed() {
+        return isDisplayed(flatShippingRate);
+    }
+
+    public boolean isPaymentMethodSectionDisplayed() {
+        return isDisplayed(termsAgreeCheckbox);
+    }
+
+    public boolean isConfirmOrderSectionDisplayed() {
+        return isDisplayed(confirmOrderBtn);
+    }
+
+    public String getConfirmOrderTotal() {
+        return getText(confirmOrderTotal);
+    }
+
+    public String getFlatShippingRateInConfirmOrder() {
+        return getText(flatShippingRateText);
+    }
+
+    public String getCartTotalFromWidget() {
+        return getText(cartTotalWidget);
+    }
+
+    public boolean isCartEmptyWidget() {
+        String cartText = getCartTotalFromWidget();
+        return cartText.contains("0 item") || cartText.contains("0 items");
     }
 }
